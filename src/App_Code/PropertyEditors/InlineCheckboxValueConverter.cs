e4ecb68c2904efe677aacbf7a360f9676813a573
leekelleher <leekelleher@gmail.com>
2018-04-06 18:15:55 +0100

    Added PropertyValueConverter and CSS for "Inline Checkbox" property-editor

---
+++ src/App_Code/PropertyEditors/InlineCheckboxValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.Web.PropertyEditors
+{
+    public class InlineCheckboxValueConverter : PropertyValueConverterBase, IPropertyValueConverterMeta
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Our.Umbraco.InlineCheckbox");
+        }
+
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            return PropertyCacheLevel.Content;
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(bool);
+        }
+    }
+}
