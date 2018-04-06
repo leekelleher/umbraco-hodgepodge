050e0a44d1440359bb7c6d591d1f83dd506f7697
leekelleher <leekelleher@gmail.com>
2018-04-06 16:43:36 +0100

    Added "Textbox Pair" property-editor

---
+++ src/App_Code/PropertyEditors/TextboxPairValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.Web.PropertyEditors
+{
+    public class TextboxPairValueConverter : PropertyValueConverterBase, IPropertyValueConverterMeta
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Our.Umbraco.TextboxPair");
+        }
+
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            return PropertyCacheLevel.Content;
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(Tuple<string, string>);
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source is string s)
+            {
+                var pair = JsonConvert.DeserializeAnonymousType(s, new { item1 = default(string), item2 = default(string) });
+                if (pair != null)
+                    return new Tuple<string, string>(pair.item1, pair.item2);
+            }
+
+            return default(Tuple<string, string>);
+        }
+    }
+}
