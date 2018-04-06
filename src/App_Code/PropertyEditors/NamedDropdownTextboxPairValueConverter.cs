f7d353972f99bf1c2dc964c3955d4828d641298b
leekelleher <leekelleher@gmail.com>
2018-04-06 16:43:03 +0100

    Added "Named Dropdown and Textbox Pair" property-editor

---
+++ src/App_Code/PropertyEditors/NamedDropdownTextboxPairValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.Web.PropertyEditors
+{
+    public class NamedDropdownTextboxPairValueConverter : PropertyValueConverterBase, IPropertyValueConverterMeta
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Our.Umbraco.NamedDropdownTextboxPair");
+        }
+
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            return PropertyCacheLevel.Content;
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(KeyValuePair<string, string>);
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source is string s)
+            {
+                var pair = JsonConvert.DeserializeAnonymousType(s, new { key = default(string), value = default(string) });
+                if (pair != null)
+                    return new KeyValuePair<string, string>(pair.key, pair.value);
+            }
+
+            return default(KeyValuePair<string, string>);
+        }
+    }
+}
---
+++ src/App_Plugins/HodgePodge/propertyeditors/named-dropdown-textbox-pair.js
@@ -0,0 +1,20 @@
+angular.module("umbraco").controller("Our.Umbraco.NamedDropdownTextboxPair.Controller",
+    function ($scope) {
+
+        //setup the default config
+        var config = {
+            items: [],
+        };
+
+        //map the user config
+        angular.extend(config, $scope.model.config);
+
+        //map back to the model
+        $scope.model.config = config;
+
+        //now we need to check if the value is null/undefined, if it is we need to set it to an empty array by default
+        if ($scope.model.value === null || $scope.model.value === undefined) {
+            $scope.model.value = [];
+        }
+
+    });
