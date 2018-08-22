246ef1b9963638077f42b7e154ac2473c4bb0360
leekelleher <leekelleher@gmail.com>
2018-08-22 08:50:25 +0100

    Named Dropdown: Streamlined the markup & logic
    I never enabled the "multiple" feature, so removing it for now (maybe some day I'll add it in)

--- src/App_Plugins/HodgePodge/propertyeditors/named-dropdown.js
+++ src/App_Plugins/HodgePodge/propertyeditors/named-dropdown.js
@@ -4,7 +4,6 @@ angular.module("umbraco").controller("Our.Umbraco.NamedDropdown.Controller",
         //setup the default config
         var config = {
             items: [],
-            multiple: false
         };
 
         //map the user config
@@ -16,12 +15,7 @@ angular.module("umbraco").controller("Our.Umbraco.NamedDropdown.Controller",
         //now we need to check if the value is null/undefined, if it is we need to set it to "" so that any value that is set
         // to "" gets selected by default
         if ($scope.model.value === null || $scope.model.value === undefined) {
-            if ($scope.model.config.multiple) {
-                $scope.model.value = [];
-            }
-            else {
-                $scope.model.value = "";
-            }
+            $scope.model.value = "";
         }
 
     });
